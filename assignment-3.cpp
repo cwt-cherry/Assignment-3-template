70232823ad7ddd026b2d52abf67286e6bc867664
cwt_cherry <wing.chan-6@student.manchester.ac.uk>
2024-02-28 16:58:39 +0000

    sample code

--- assignment-3.cpp
+++ assignment-3.cpp
@@ -1,70 +1,135 @@
-// PHYS 30762 Programming in C++
-// Assignment 3
-// Practice with C++ classes and their interactions
+// // PHYS 30762 Programming in C++
+// // Assignment 3
+// // Practice with C++ classes and their interactions
 
-#include<iostream>
-#include<string>
-#include<vector>
-#include<cmath>
+// #include<iostream>
+// #include<string>
+// #include<vector>
+// #include<cmath>
 
-using std::string;
+// using std::string;
 
-// Beginning of particle class
-class particle
-{
+// // Beginning of particle class
+// class particle
+// {
+// private:
+//   string particle_name;
+//   //...other data members (see slides on BB)
+
+// public:
+//   // Constructors
+//   // Here you need a default constructor, and a parameterised constructor
+
+//   // Destructor
+
+//   // Getter functions (accessors) to 
+//   // This should include function returning beta value 
+
+//   // Setter functions, to change value of data members
+//   // Make sure you check input validity before changing something
+//   // Hint: you can use the input checking functions you used in assignment 1
+
+//   // Function to print info about a particle
+//   void print_data();
+
+// };
+
+// // Implementation of print_data function goes here
+
+// // End of particle class and associated member functions
+
+// // Beginning of detector class
+
+// // Functionalities needed, in addition to constructor/destructor/setters/getters (see slides on BB):
+// // - write a function that takes a particle 
+// //   and returns a bool and a print-out on whether this type of detector detected the particle
+// //   depending on the detector/particle combination (see slides on BB)
+// // - have data members that keep track of how many particles were detected by this particular detector, 
+// //   and of which type
+// // - write a function (accessor) that prints how many particles passed through this detector
+
+// // End of detector class
+
+// // Main program
+// int main()
+// {
+
+//   // Create the following particles: 
+//   // two electrons, four muons, three taus, one antielectron, one antimuon, one antitau 
+//   // Use the parameterised constructor
+
+//   // Print out the data from all the particles (put them in a vector)
+
+//   // Create the following detectors: a tracker, a calorimeter, a muon chamber
+
+//   // Pass the list of particles into each detector
+
+//   // Print a summary of how many particles were detected
+
+//   return 0;
+// }
+
+
+#include <iostream>
+#include <string>
+#include <cmath>
+
+class Lepton {
 private:
-  string particle_name;
-  //...other data members (see slides on BB)
+    std::string particleType;
+    double restMass;  // in MeV
+    int charge;       // +1 for particles, -1 for antiparticles
+    double velocity;   // in m/s
+
+    // Constant for the speed of light
+    static const double speedOfLight;
 
 public:
-  // Constructors
-  // Here you need a default constructor, and a parameterised constructor
+    // Constructor
+    Lepton(std::string type, double mass, int charge, double velocity)
+        : particleType(type), restMass(mass), charge(charge) {
+        setVelocity(velocity);
+    }
 
-  // Destructor
+    // Getter functions
+    std::string getParticleType() const { return particleType; }
+    double getRestMass() const { return restMass; }
+    int getCharge() const { return charge; }
+    double getVelocity() const { return velocity; }
+    double getBeta() const { return velocity / speedOfLight; }
 
-  // Getter functions (accessors) to 
-  // This should include function returning beta value 
+    // Setter function for velocity with input checking
+    void setVelocity(double newVelocity) {
+        if (newVelocity >= 0 && newVelocity <= speedOfLight) {
+            velocity = newVelocity;
+        } else {
+            std::cerr << "Invalid velocity. Velocity should be between 0 and the speed of light." << std::endl;
+        }
+    }
 
-  // Setter functions, to change value of data members
-  // Make sure you check input validity before changing something
-  // Hint: you can use the input checking functions you used in assignment 1
-
-  // Function to print info about a particle
-  void print_data();
-
+    // Function to display lepton information
+    void displayLeptonInfo() const {
+        std::cout << "Particle Type: " << particleType << std::endl;
+        std::cout << "Rest Mass: " << restMass << " MeV" << std::endl;
+        std::cout << "Charge: " << charge << std::endl;
+        std::cout << "Velocity: " << velocity << " m/s" << std::endl;
+        std::cout << "Beta: " << getBeta() << std::endl;
+    }
 };
 
-// Implementation of print_data function goes here
+// Initialization of the speed of light constant
+const double Lepton::speedOfLight = 2.99792458e8;
 
-// End of particle class and associated member functions
+int main() {
+    // Example usage
+    Lepton electron("Electron", 0.511, -1, 2.0e7); // Electron at 2.0e7 m/s
 
-// Beginning of detector class
+    // Display lepton information
+    electron.displayLeptonInfo();
 
-// Functionalities needed, in addition to constructor/destructor/setters/getters (see slides on BB):
-// - write a function that takes a particle 
-//   and returns a bool and a print-out on whether this type of detector detected the particle
-//   depending on the detector/particle combination (see slides on BB)
-// - have data members that keep track of how many particles were detected by this particular detector, 
-//   and of which type
-// - write a function (accessor) that prints how many particles passed through this detector
+    // Change the velocity and display information again
+    electron.setVelocity(3.0e8); // Attempting to set velocity greater than the speed of light
+    electron.displayLeptonInfo();
 
-// End of detector class
-
-// Main program
-int main()
-{
-
-  // Create the following particles: 
-  // two electrons, four muons, three taus, one antielectron, one antimuon, one antitau 
-  // Use the parameterised constructor
-
-  // Print out the data from all the particles (put them in a vector)
-
-  // Create the following detectors: a tracker, a calorimeter, a muon chamber
-
-  // Pass the list of particles into each detector
-
-  // Print a summary of how many particles were detected
-
-  return 0;
+    return 0;
 }
