b14ea527bcb9b86daa7db8779ea7553abd47aafe
cwt_cherry <wing.chan-6@student.manchester.ac.uk>
2024-03-04 13:03:41 +0000

    invalid velocity

---
+++ detector.h
@@ -0,0 +1,114 @@
+#ifndef DETECTOR_H
+#define DETECTOR_H
+#include "particle.h"
+
+// Beginning of detector class
+class detector
+{
+private:
+  string type;
+  bool status;
+  int particles_count;
+
+public:
+  // Destruct Constructor
+  detector(): type{}, status{}
+  {
+  }
+
+  // Parameterised Constructor
+  detector(string chosen_type, bool chosen_status):
+    type{chosen_type}, status{chosen_status}
+  {
+  }
+
+  // Destructor
+  ~detector()
+  {
+    // std::cout<<"Destroying "<<type<<std::endl;
+  }
+
+  // Setter
+  void turn_on()
+  {
+    status = true;
+  }
+  void turn_off()
+  {
+    status = false;
+  }
+
+  // Ask user to enter detector status
+  bool set_status(string type)
+  {
+    string chosen_status;
+    while(true)
+    {
+      std::cout<<"Enter status of "<<type<<":"<<std::endl;
+      std::cin >> chosen_status;
+      if (chosen_status == "on")
+      {
+        turn_on();
+        return true;
+      }
+      else if (chosen_status == "off")
+      {
+        turn_off();
+        return false;
+      }
+      else
+      {
+          std::cin.clear();
+          std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+          std::cout << "Invalid input. Please enter choice again." << std::endl;
+      }
+    }
+  }
+
+//   // Getter
+//   bool get_status()
+//   {
+//     return status;
+//   }
+
+  // Function to detect particles
+  bool detect_particles(particle detected_particle)
+  {
+    if(status)
+    {
+      if(type=="tracker"&&(detected_particle.get_name()=="electron"||detected_particle.get_name()=="antielectron"||detected_particle.get_name()=="muon"||detected_particle.get_name()=="antimuon"))
+      {
+        std::cout<<detected_particle.get_name()<<" was detected in the tracker."<<std::endl;
+        particles_count++;
+        return true;
+      }
+      else if(type=="calorimeter"&&(detected_particle.get_name()=="electron"||detected_particle.get_name()=="antielectron"))
+      {
+        std::cout<<detected_particle.get_name()<<" was detected in the calorimeter."<<std::endl;
+        particles_count++;
+        return true;
+      }
+      else if(type=="muon_chamber"&&(detected_particle.get_name()=="muon"||detected_particle.get_name()=="antimuon"))
+      {
+        std::cout<<detected_particle.get_name()<<" was detected in the muon chamber."<<std::endl;
+        particles_count++;
+        return true;
+      }
+      return false;
+    }
+    else
+    {
+      std::cout<<"No particles were detected in the "<<type<<" ."<<std::endl;
+      return false;
+    }
+  }
+
+// Accessor that prints how many particles passed through this detector
+  void print_detected_results()
+  {
+    std::cout<<"Number of particles passed through "<<type<<": "<<particles_count<<std::endl;
+  }
+};
+// End of detector class
+
+#endif
---
+++ particle.h
@@ -0,0 +1,85 @@
+#ifndef PARTICLE_H
+#define PARTICLE_H
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<cmath>
+
+using std::string;
+
+// Beginning of particle class
+class particle
+{
+private:
+  string name; // electron or muon
+  double rest_mass; // Unit of MeV
+  int charge; // -1 or +1
+  const double speed_of_light = 2.9979245e8; // Unit of m/s
+  double velocity; // Between 0 and c, in m/s
+  double beta; // Between 0-1
+
+public:
+  // Destruct Constructor
+  particle(): name{}, rest_mass{}, charge{}, velocity{}, beta{}
+  {
+  }
+
+  // Parameterised Constructor
+  particle(string particle_name, double particle_rest_mass, int particle_charge, double particle_velocity):
+    name{particle_name}, rest_mass{particle_rest_mass}, charge{particle_charge}, velocity{particle_velocity}, beta{}
+  {
+    set_velocity(particle_velocity);
+    beta = velocity / speed_of_light;
+  }
+
+  // Destructor
+  ~particle()
+  {
+    // std::cout<<"Destroying "<<name<<std::endl;
+  }
+
+  // Getter functions
+  string get_name() {return name;}
+
+//   double get_rest_mass()
+//   {
+//     return rest_mass;
+//   }
+//   int get_charge()
+//   {
+//     return charge;
+//   }
+//   double get_beta()
+//   {
+//     return beta;
+//   }
+
+  // Setter functions
+  double set_velocity(double particle_velocity)
+  {
+    if (speed_of_light >= particle_velocity && particle_velocity >= 0)
+    {
+      return velocity = particle_velocity;
+    }
+    else
+    {
+      std::cout<<"Invalid velocity. Velocity should be between 0 and the speed of light."<<std::endl;
+      exit(0);
+    }
+  }
+  // Function to print info about a particle
+  void print_data()
+  {
+    std::cout<<"particle type: "<<name<<std::endl;
+    std::cout<<"rest mass: "<<rest_mass<<std::endl;
+    std::cout<<"charge: "<<charge<<std::endl;
+    std::cout<<"velocity: "<<velocity<<std::endl;
+    std::cout<<"beta value: "<<beta<<std::endl;
+    std::cout<<"---------------------------------"<<std::endl;
+  }
+
+};
+// End of particle class and associated member functions
+
+#endif
